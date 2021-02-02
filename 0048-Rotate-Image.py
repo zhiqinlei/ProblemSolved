a61a5f20b93d341abf3a082a2949b37a908b7998
Zhiqin Lei <zhiqinlei1998@gmail.com>
2021-02-01 21:08:44 -0800

    Flip flip

---
+++ 0048-Rotate-Image.py
@@ -0,0 +1,11 @@
+# 0048-Rotate-Image.py
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()
+        print(matrix)
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
