9ae4f796f2271a13df4eb157d2ef9bbf69e9dd5b
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-02-08 16:50:52 -0800

    damn subscribe

---
+++ 0277-Find-the-Celebrity.py
@@ -0,0 +1,25 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+'''
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n). There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+'''
+
+class Solution(object):
+    def findCelebrity(self, n):
+        C = 0 # C is the candidate
+        for i in range(1, n): 
+            if knows(C, i): # if anyone knows C, C cannot be the celebrity
+                C = i # now C is the one dont know anyone
+        for j in range(n): # if C knows anyone or someone dont know C
+            if (j != C and knows(C, j)) or not knows(j, C):
+                return -1 # none is Celebrity
+        return C # else the candidate is the celebrity
