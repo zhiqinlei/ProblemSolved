4aa8bfdce21ff55de22268508dad930eb4509a89
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-02-10 15:53:54 -0800

    only simple

---
+++ 0041-First-Missing-Positive.py
@@ -0,0 +1,34 @@
+# O(nlgn)
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        small = 1
+        for i in nums:
+            if i == small:
+                small += 1
+        return small
+
+#O(n) time, O(1) space, quite brilliant
+
+def firstMissingPositive(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+     Basic idea:
+    1. for any array whose length is l, the first missing positive must be in range [1,...,l+1], 
+        so we only have to care about those elements in this range and remove the rest.
+    2. we can use the array index as the hash to restore the frequency of each number within 
+         the range [1,...,l+1] 
+    """
+    nums.append(0)
+    n = len(nums)
+    for i in range(len(nums)): #delete those useless elements
+        if nums[i]<0 or nums[i]>=n:
+            nums[i]=0
+    for i in range(len(nums)): #use the index as the hash to record the frequency of each number
+        nums[nums[i]%n]+=n
+    for i in range(1,len(nums)):
+        if nums[i]/n==0:
+            return i
+    return n
