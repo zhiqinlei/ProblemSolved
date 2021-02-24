70b46f28ced0ff0e53665642d80abd8e5c17f46c
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-02-23 16:48:58 -0800

    get job at google

---
+++ 0226-Invert-Binary-Tree.py
@@ -0,0 +1,45 @@
+# recursively
+def invertTree1(self, root):
+    if root:
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+        
+# BFS
+def invertTree2(self, root):
+    queue = collections.deque([(root)])
+    while queue:
+        node = queue.popleft()
+        if node:
+            node.left, node.right = node.right, node.left
+            queue.append(node.left)
+            queue.append(node.right)
+    return root
+    
+# DFS
+def invertTree(self, root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack.extend([node.right, node.left])
+    return root
+
+'''
+If you do root.left = invert(root.right) first, then you lost the previous left subtree. Of course that can't work. Also, please format multi-line code properly, especially Python code, which depends on indentation.
+
+def invertTree(self, root):
+    if root:
+        invert = self.invertTree
+        root.left, root.right = invert(root.right), invert(root.left)
+        return root
+
+def invertTree(self, root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack += node.left, node.right
+    return root
+'''
