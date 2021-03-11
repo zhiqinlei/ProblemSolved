128d01f8c967506667c88949a873eef45eeda06b
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-03-10 21:52:32 -0800

    slow & fast pointer

--- 0141-Linked-List-Cycle.py
+++ 0141-Linked-List-Cycle.py
@@ -4,12 +4,13 @@
 #         self.val = x
 #         self.next = None
 
+# 快慢指针，腾讯面试
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         slow = fast = head
         while fast and fast.next:
             fast = fast.next.next
-            slow = slow.next
+            slow = slow.next 
             if slow is fast:
                 return True
         return False
