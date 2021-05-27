bf179b3cc8040164571810fefe3762e2b5917af3
zhiqinlei <zhiqinlei1998@gmail.com>
2021-05-27 22:16:53 +0800

    pre.next = head

---
+++ 0024-Swap-Nodes-in-Pairs.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummy = pre = ListNode(0)
+        pre.next = head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = pre.next.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = pre.next.next
+        return dummy.next
+    
+***
+To go from pre -> a -> b -> b.next to pre -> b -> a -> b.next, we need to change those three references. Instead of thinking about in what order I change them, I just change all three at once.
+***
