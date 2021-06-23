d60681ae60a7014dbfa04b72777853a596e7043b
zhiqinlei <zhiqinlei1998@gmail.com>
2021-06-23 21:42:36 +0800

    duplicate & nonduplicate

--- 0035-Search-Insert-Position.py
+++ 0035-Search-Insert-Position.py
@@ -9,3 +9,17 @@ def searchInsert(self, nums: List[int], target: int) -> int:
             else:
                 right = mid -1
         return left
+
+# with duplicate
+def searchInsert(self, nums, target): # works even if there are duplicates. 
+    l , r = 0, len(nums)-1
+    while l <= r:
+        mid=(l+r)/2
+        if nums[mid] < target:
+            l = mid+1
+        else:
+            if nums[mid]== target and nums[mid-1]!=target:
+                return mid
+            else:
+                r = mid-1
+    return l
