9fc43f368837c4e6c1786b1eca835a967ddf1489
zhiqinlei <zhiqinlei1998@gmail.com>
2021-06-28 22:54:07 +0800

    use matrix property

---
+++ 0378-Kth-Smallest-Element-in-a-Sorted-Matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        
+        def checkmid(mid):
+            i, j = n-1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += i + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+        
+        left, right = matrix[0][0], matrix[-1][-1]
+        while left < right:
+            mid = (left+right)//2
+            if checkmid(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
