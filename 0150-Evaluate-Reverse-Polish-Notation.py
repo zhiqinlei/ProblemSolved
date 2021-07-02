ebc824c4b8b9744c7496deddf92bf2444cba8a19
zhiqinlei <zhiqinlei1998@gmail.com>
2021-07-03 00:52:40 +0800

    not //

---
+++ 0150-Evaluate-Reverse-Polish-Notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            if i not in '+-*/':
+                stack.append(int(i))
+                
+            else:
+                num2 = int(stack.pop())
+                num1 = int(stack.pop())
+                if i == "+":
+                    stack.append(num1+num2)
+                if i == "-":
+                    stack.append(num1-num2)
+                if i == "*":
+                    stack.append(num1*num2)
+                if i == "/":
+                    stack.append(int(float(num1/num2))) # not // will truncate to zero
+        
+        return stack.pop()
