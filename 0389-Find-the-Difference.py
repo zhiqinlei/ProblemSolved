dcf9a5d5f65030c065933200c783eaf9e93cb340
zhiqinlei <zhiqinlei1998@gmail.com>
2021-07-05 22:15:54 +0800

    three methods

--- 0389-Find-the-Difference.py
+++ 0389-Find-the-Difference.py
@@ -6,3 +6,27 @@ class Solution:
             diff += ord(t[i])
         diff += ord(t[-1])
         return chr(diff)
+"""
+    dictionary
+    
+    def findTheDifference(self, s, t):
+        dic = {}
+        for ch in s:
+            dic[ch] = dic.get(ch, 0) + 1
+        for ch in t:
+            if dic.get(ch, 0) == 0:
+                return ch
+            else:
+                dic[ch] -= 1
+                """
+"""
+class Solution(object):
+    """
+    xor
+    """
+    def findTheDifference(self, s, t):
+        code = 0
+        for ch in s + t:
+            code ^= ord(ch)
+        return chr(code)
+"""
