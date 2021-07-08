dab4dcf3849a52089b58b2312032307abf182d91
zhiqinlei <zhiqinlei1998@gmail.com>
2021-07-08 21:16:34 +0800

    random.randrange

---
+++ 0384-Shuffle-an-Array.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+         self.nums = nums
+        
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        ans = self.nums[:]
+        for i in range(len(ans)):
+            j = random.randrange(i+1)
+            ans[i],ans[j] = ans[j],ans[i]
+        return ans
+            
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
