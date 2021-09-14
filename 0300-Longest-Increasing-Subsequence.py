ae3c1a3d310f6e12cdb5d6ec99ec361b942040b4
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-09-14 15:07:11 -0700

    dp or binary search

--- 0300-Longest-Increasing-Subsequence.py
+++ 0300-Longest-Increasing-Subsequence.py
@@ -1,5 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # cn lc solution
+        # use greedy and binary search to solve
+        res = []
+        
+        for n in nums:
+            if not res or n > res[-1]: # if find a num larger than the last element, just add
+                res.append(n)
+            else: # if not larger than the last, check if it is smaller than any element in res by using binary search
+                left, right = 0, len(res)-1
+                idx = right # idx of element larger than n, if not found, replace the last one
+                while left <= right: # left <= right not <
+                    mid = (left + right)//2
+                    if res[mid] >= n: # find idx in res, not nums, must be >=
+                        idx = mid
+                        right = mid -1 # mid -1 not -=1
+                    else:
+                        left = mid + 1
+                res[idx] = n # replace it
+        return len(res)
+    
+    # O(nlogn) Time, O(n) Space
+    class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
         # lc cn solution, we can use dp to solve this problem
         # https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
         # or patient sorting soltuion: https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
