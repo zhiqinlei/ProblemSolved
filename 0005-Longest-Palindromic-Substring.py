ff79b15bab890d3fe3ce757b2d2a8c59e0e53ade
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-09-21 17:56:03 -0700

    dp solution

--- 0005-Longest-Palindromic-Substring.py
+++ 0005-Longest-Palindromic-Substring.py
@@ -1,4 +1,5 @@
 class Solution:
+    # similar to dp solution
     def longestPalindrome(self, s: str) -> str:
         ans = ''
         for i in range(len(s)):
@@ -14,4 +15,7 @@ class Solution:
         while left >=0 and right <len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-        return s[left+1:right]
+        return s[left+1:right] # slice stop at right -1
+        #The while loop stops either because l or r is out of range or because the s[l] != s[r], which means neither s[l] nor s[r] can be part of the longest palindrome and the helper function returns s[l+1:r](inclusive on the left and exclusive on the right).
+
+        # Time O(n2) Space O1
