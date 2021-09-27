0bcdcba21b3d490020e3107ce72677d94bf27258
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-09-26 22:37:07 -0700

    update

--- ExtraProblems/Amazon/Prime-Numbers(countPrimeStrings).py
+++ ExtraProblems/Amazon/Prime-Numbers(countPrimeStrings).py
@@ -15,3 +15,29 @@ For Custom Testing
 Sample Output Explanation
 The 3 ways to split this string into prime numbers are (31, 7,5), (3, 17, 5), (317,5)
 """
+
+def isPrime(number):
+    num = number
+    i = 2
+    while i * i <= num:
+        if ((num % i) == 0):
+            return False
+        i += 1
+    return num > 1
+
+def countPrimeStrings(number, i):
+    if (i == 0):
+        return 1
+    cnt = 0
+    for j in range(1, i + 1):
+        if (number[i - j] != '0' and isPrime(int(number[i - j: i]))):
+            cnt += countPrimeStrings(number, i - j)
+    return cnt
+
+if __name__ == "__main__":
+    s1 = "3175"
+    l1 = len(s1)
+    print(countPrimeStrings(s1, l1))
+    s2 = "11373"
+    l2 = len(s2)
+    print(countPrimeStrings(s2, l2))
